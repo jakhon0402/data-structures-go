3a6f8703e4fa7ceede022f96d3380b2065a25398
JAKHONGIR <jakhon99dev@gmail.com>
2023-05-06 15:58:51 +0500

    tries

--- tries/main.go
+++ tries/main.go
@@ -1,8 +1,55 @@
-package tries
+package main
 
 import "fmt"
 
+const AlphabetSize = 26
+
+type Node struct {
+	children [AlphabetSize]*Node
+	isEnd    bool
+}
+
+type Trie struct {
+	root *Node
+}
+
+func InitTrie() *Trie {
+	result := &Trie{root: &Node{}}
+	return result
+}
+
+func (t *Trie) Insert(w string) {
+	wordLength := len(w)
+	currentNode := t.root
+	for i := 0; i < wordLength; i++ {
+		charIndex := w[i] - 'a'
+		if currentNode.children[charIndex] == nil {
+			currentNode.children[charIndex] = &Node{}
+		}
+		currentNode = currentNode.children[charIndex]
+	}
+	currentNode.isEnd = true
+}
+
+func (t *Trie) Search(w string) bool {
+	wordLength := len(w)
+	currentNode := t.root
+	for i := 0; i < wordLength; i++ {
+		charIndex := w[i] - 'a'
+		if currentNode.children[charIndex] == nil {
+			return false
+		}
+		currentNode = currentNode.children[charIndex]
+	}
+	if currentNode.isEnd {
+		return true
+	}
+	return false
+}
+
 func main() {
-	fmt.Println("Biror narsa")
-	fmt.Println("HAHAHAH")
+	myTrie := InitTrie()
+	myTrie.Insert("gorgeous")
+	fmt.Println(myTrie.root)
+	fmt.Println(myTrie.Search("gorgeous"))
 }
